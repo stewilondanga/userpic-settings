4d7bebb9344bf5a1d9f7cd039b486621a20eb06d
Stewart ilondanga <stilondanga@gmail.com>
2019-06-11 16:38:10 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -12,10 +12,10 @@ const messageElement = document.querySelector('#js-message');
 // Image elements
 const fileUpload = document.querySelector('#js-file-uploader');
 const profileTrigger = document.querySelector('#js-profile-trigger');
-/*const profileBackground = document.querySelector('#js-profile-pic');
+const profileBackground = document.querySelector('#js-profile-pic');
 
 // Password objects
-const password        = document.querySelector('#js-password');
+/*const password        = document.querySelector('#js-password');
 const passwordConfirm = document.querySelector('#js-password-confirm');
 const passwordToggle  = document.querySelector('#js-toggle-password');
 const passwordSuggest = document.querySelector('#js-suggest-password');
